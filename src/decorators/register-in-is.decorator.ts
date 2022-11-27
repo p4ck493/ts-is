b2f7b396cac7d5f2a4f09a7168f5f7824ff56e43
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2022-11-27 14:13:23 +0100

    2.0.4

--- src/decorators/register-in-is.decorator.ts
+++ src/decorators/register-in-is.decorator.ts
@@ -1,35 +1,36 @@
 import {
-    registerInIsArgumentDecoratorType,
-    registerInIsConstructorDecoratorType,
+  registerInIsArgumentDecoratorType,
+  registerInIsConstructorDecoratorType,
 } from '../types/decorators/register-in-is.decorator.type';
-import {RegisterInIsDecoratorInterface} from '../interfaces/decorators/register-in-is.decorator.interface';
-import {methods} from '../engine/methods';
+import { RegisterInIsDecoratorInterface } from '../interfaces/decorators/register-in-is.decorator.interface';
+import { methods } from '../engine/methods';
 
-export function registerInIsDecorator(configuration?: RegisterInIsDecoratorInterface): any { // TODO interface
-    return (constructor: registerInIsConstructorDecoratorType): void => {
-        let value: any = constructor; // TODO interface
-        if (configuration?.customMethod) {
-            value = value?.[configuration.customMethod];
-            if (!value) {
-                throw new Error(`Not found customMethod with name: ${configuration.customMethod}`);
-            }
-        } else {
-            value = () => {
-                return {
-                    classRef: constructor,
-                };
-            };
-        }
-        let className: string | undefined = configuration?.className;
-        if (!className) {
-            const object: registerInIsArgumentDecoratorType = new constructor();
-            className = object.constructor.name;
-        }
-        Object.defineProperty(methods, className, {
-            configurable: true,
-            enumerable: true,
-            writable: true,
-            value,
-        });
-    };
+export function registerInIsDecorator(configuration?: RegisterInIsDecoratorInterface): any {
+  // TODO interface
+  return (constructor: registerInIsConstructorDecoratorType): void => {
+    let value: any = constructor; // TODO interface
+    if (configuration?.customMethod) {
+      value = value?.[configuration.customMethod];
+      if (!value) {
+        throw new Error(`Not found customMethod with name: ${configuration.customMethod}`);
+      }
+    } else {
+      value = () => {
+        return {
+          classRef: constructor,
+        };
+      };
+    }
+    let className: string | undefined = configuration?.className;
+    if (!className) {
+      const object: registerInIsArgumentDecoratorType = new constructor();
+      className = object.constructor.name;
+    }
+    Object.defineProperty(methods, className, {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value,
+    });
+  };
 }

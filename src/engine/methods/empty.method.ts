e35e26c6c48173778db4a5f688b9c2a778fc0163
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2022-12-04 19:10:41 +0100

    3.0.2

--- src/engine/methods/empty.method.ts
+++ src/engine/methods/empty.method.ts
@@ -1,23 +1,23 @@
-import {StringMethod} from './string.method';
-import {ObjectMethod} from './object.method';
-import {ArrayMethod} from './array.method';
+import { StringMethod } from './string.method';
+import { ObjectMethod } from './object.method';
+import { ArrayMethod } from './array.method';
 
 export function EmptyMethod<T extends object>(argument: unknown): boolean {
-    if (StringMethod(argument)) {
-        return (argument.trim())[0] === undefined
-    }
+  if (StringMethod(argument)) {
+    return argument.trim()[0] === undefined;
+  }
 
-    if (ObjectMethod<T>(argument) || ArrayMethod<T>(argument)) {
-        if (Reflect.has(argument, 'size')) {
-            // @ts-ignore
-          return argument.size <= 0;
-        }
-        for (const key in argument) {
-            if (argument.hasOwnProperty(key)) {
-                return false;
-            }
-        }
-        return true;
+  if (ObjectMethod<T>(argument) || ArrayMethod<T>(argument)) {
+    if (Reflect.has(argument, 'size')) {
+      // @ts-ignore
+      return argument.size <= 0;
     }
-    return false;
+    for (const key in argument) {
+      if (argument.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }

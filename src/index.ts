0bed085a5c0abf89aec59c8fd887c64a4dc3c8ef
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2022-12-04 09:51:59 +0100

    3.0.1-beta.4

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import {proxyGenerator} from './engine/proxy/generator';
-import {IsType} from './types/is.type';
+import { proxyGenerator } from './engine/proxy/generator';
+import { IsType } from './types/is.type';
 
-import {registerInIsDecorator} from './decorators/register-in-is.decorator';
+import { registerInIsDecorator } from './decorators/register-in-is.decorator';
 
 export const RegisterInIs = registerInIsDecorator;
-
 
 function defaultFunction(argument: unknown): unknown {
   // TODO return typeof argument or make something cool

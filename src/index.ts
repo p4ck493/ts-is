4b6f1066eda7d1f89ab91dfde07cbd40037262bd
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-02-03 00:25:08 +0100

    Clear

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import { proxyGenerator } from './proxy/generator';
-import { registerInIsDecorator } from './decorators/register-in-is.decorator';
-import { AllMethodsInterface } from './methods';
+import {registerInIsDecorator} from './decorators/register-in-is.decorator';
+import {AllMethodsInterface} from './methods';
+import {proxyGeneratorGet} from './proxy/generator/get.generator.proxy.engine';
 
 export const RegisterInIs = registerInIsDecorator;
 
 function defaultFunction(argument: unknown): unknown {
-  // TODO return typeof argument or make something cool
-  return argument;
+    // TODO return typeof argument or make something cool
+    return argument;
 }
 
-export const is: AllMethodsInterface = proxyGenerator(defaultFunction);
+export const is: AllMethodsInterface = new Proxy<any>(defaultFunction, {
+    get: proxyGeneratorGet(),
+    apply: (
+        target: (...arg: unknown[]) => boolean,
+        thisArg: unknown,
+        argumentList: unknown[],
+    ): ReturnType<typeof target> => {
+        return target(...argumentList);
+    },
+});

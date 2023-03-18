4d7a315ae197868121c59a4ad1b09abbc8dbf4ee
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-18 16:21:45 +0100

    Clear

--- src/methods/index.ts
+++ src/methods/index.ts
@@ -43,8 +43,6 @@ export type ConvertTypeToGenericInstanceOf<T> = {
   [key in keyof T]: instanceofTypeMix<T[key]>;
 };
 
-// A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z.
-
 export const predefinedMethods = {
   array: ArrayMethod,
   asyncFunction: AsyncFunctionMethod,

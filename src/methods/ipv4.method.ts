f9d8916ba07e85f63f77b46f66a3c1c6c8b607c7
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-18 12:41:41 +0100

    Delete global mode from regex

--- src/methods/ipv4.method.ts
+++ src/methods/ipv4.method.ts
@@ -4,5 +4,6 @@ export function Ipv4Method(argument: string): argument is string {
     if (SymbolMethod(argument)) {
         return false;
     }
-    return /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/gm.test(argument);
+    return /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(argument);
 }
+
